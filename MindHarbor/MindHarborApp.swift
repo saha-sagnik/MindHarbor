ec29d138fa1daf7d4947c39f39b9ac44375676e5
Sagnik Saha <sagnik12347@gmail.com>
2025-07-16 00:09:45 +0530

    Initial Commit

---
+++ MindHarbor/MindHarborApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindHarborApp.swift
+//  MindHarbor
+//
+//  Created by Sagnik Saha on 16/07/25.
+//
+
+import SwiftUI
+
+@main
+struct MindHarborApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
